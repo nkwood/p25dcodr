01efcd26a01cb82be0689a22e39c70791f365223
Rhodey Orbits <rhodey@anhonesteffort.org>
2016-03-14 12:34:36 -0500

    terminator data units are used strangely

--- src/main/java/org/anhonesteffort/p25/protocol/GroupTrafficChannelCapture.java
+++ src/main/java/org/anhonesteffort/p25/protocol/GroupTrafficChannelCapture.java
@@ -20,15 +20,8 @@ package org.anhonesteffort.p25.protocol;
 import org.anhonesteffort.kinesis.producer.KinesisRecordProducer;
 import org.anhonesteffort.p25.kinesis.KinesisDataUnitSink;
 import org.anhonesteffort.p25.model.ChannelId;
-import org.anhonesteffort.p25.protocol.frame.DataUnit;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class GroupTrafficChannelCapture extends KinesisDataUnitSink {
-
-  private static final Logger log = LoggerFactory.getLogger(GroupTrafficChannelCapture.class);
-
-  private final ChannelId channelId;
 
   public GroupTrafficChannelCapture(KinesisRecordProducer sender,
                                     ChannelId             channelId,
@@ -36,22 +29,6 @@ public class GroupTrafficChannelCapture extends KinesisDataUnitSink {
                                     Double                srcLongitude)
   {
     super(sender, channelId, srcLatitude, srcLongitude);
-    this.channelId = channelId;
-  }
-
-  @Override
-  public void consume(DataUnit element) {
-    super.consume(element);
-    if (!element.isIntact())
-      return;
-
-    switch (element.getNid().getDuid().getId()) {
-      case Duid.ID_TERMINATOR_W_LINK:
-      case Duid.ID_TERMINATOR_WO_LINK:
-        log.info(channelId + " terminated by protocol, canceling");
-        Thread.currentThread().interrupt();
-        break;
-    }
   }
 
 }
